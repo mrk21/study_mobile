0e4dfa1c1e851307fc2e4c0b9db4a3b11b0de1ec
Yuichi Murata <mrk21info+github@gmail.com>
2016-02-06 23:31:21 +0900

    swift_operator: add

---
+++ swift_operator/README.md
@@ -0,0 +1,5 @@
+# Swift operator
+
+## Refer to
+
+* [数値型 | Swift言語を学ぶ](http://tea-leaves.jp/swift/content/数値型)
---
+++ swift_operator/swift_operator.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+print(1+2) // addition
+print(5-3) // subtraction
+print(2*3) // multiplication
+print(4/2) // division
+print(5%2) // residue
+
+var value_operation1 = 3
+print(++value_operation1)  // increment
+print(value_operation1++)  // increment
+value_operation1+=2; print(value_operation1) // increment
+print(--value_operation1)  // decrement
+print(value_operation1--)  // decrement
+value_operation1-=3; print(value_operation1) // decrement
