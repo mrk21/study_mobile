341abb3932f9a57cb1401fa2407affed36b3c0af
Yuichi Murata <mrk21info+github@gmail.com>
2016-02-08 23:57:47 +0900

    swift_operator: add a code of conparsion operators

--- swift_operator/swift_operator.playground/Contents.swift
+++ swift_operator/swift_operator.playground/Contents.swift
@@ -16,8 +16,31 @@ print(--value_operation1)  // decrement
 print(value_operation1--)  // decrement
 value_operation1-=3; print(value_operation1) // decrement
 
+
 // Overflow operator
 // It allows to overflow the value of a variable.
 var value_operator2 = Int8.max
 // print(value_operator2 + 1) // error: overflow
 print(value_operator2 &+ 1) // OK: -128
+
+
+// Conparsion operator
+print(2 == 3)
+print(2 != 3)
+print(2 >  3)
+print(2 <  3)
+print(2 >= 3)
+print(2 <= 3)
+
+print(1 == "1")
+print(nil == false)
+print(0 == nil)
+
+print(0 ==  false)
+print(0 === false)
+print(1 ==  true)
+print(1 === true)
+print(0 !=  false)
+print(0 !== false)
+print(1 !=  true)
+print(1 !== true)
