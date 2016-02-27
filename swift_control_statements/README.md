cc86949681822cd01ea9a068d9ed5574d60b4f29
Yuichi Murata <mrk21info+github@gmail.com>
2016-02-27 23:52:54 +0900

    swift_control_statements: add

---
+++ swift_control_statements/README.md
@@ -0,0 +1,5 @@
+# Swift Control Statements
+
+## Refer to
+
+* [制御文 | Swift言語を学ぶ](http://tea-leaves.jp/swift/content/制御文)
---
+++ swift_control_statements/swift_control_statements.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// if
+var value_if1 = 100
+
+if value_if1 <= 50 {
+    print("1")
+}
+else if value_if1 <= 125 {
+    print("2")
+}
+else {
+    print("3")
+}
