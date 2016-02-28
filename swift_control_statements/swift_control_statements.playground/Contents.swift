9129fbd7b235c3c19e9ec2b35825d5ecdc8dfd3e
Yuichi Murata <mrk21info+github@gmail.com>
2016-02-28 23:56:57 +0900

    swift_control_statements: add a code of for statement

--- swift_control_statements/swift_control_statements.playground/Contents.swift
+++ swift_control_statements/swift_control_statements.playground/Contents.swift
@@ -12,3 +12,15 @@ else if value_if1 <= 125 {
 else {
     print("3")
 }
+
+// for
+var value_for1 = [1,2,3]
+for var i=0; i<value_for1.count; ++i {
+    print(value_for1[i])
+}
+for var i=0; ; ++i {
+    if i >= value_for1.count { break }
+    print(value_for1[i])
+    if i < value_for1.count { continue }
+    print("This message is not displayed.")
+}
