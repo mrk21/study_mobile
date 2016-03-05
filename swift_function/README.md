d4349cb661b7c9e75c189b633c16dd2408a923b7
Yuichi Murata <mrk21info+github@gmail.com>
2016-03-05 23:58:05 +0900

    swift_function: add

---
+++ swift_function/README.md
@@ -0,0 +1,5 @@
+# Swift Function
+
+## Refer to
+
+* [関数 | Swift言語を学ぶ](http://tea-leaves.jp/swift/content/関数)
---
+++ swift_function/swift_function.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func func1(){
+    print("a")
+}
+func1()
+
+func func2(v: String){
+    print(v)
+}
+func2("a")
+
+func func3(v: String) -> String {
+    return "v: \(v)"
+}
+print(func3("a"))
