b5bbeb4d5434b1f8f839d6d9e44744aec95d607f
Yuichi Murata <mrk21info+github@gmail.com>
2016-03-08 23:59:21 +0900

    swift_function: add a code of constant parameters and variable parameters

--- swift_function/swift_function.playground/Contents.swift
+++ swift_function/swift_function.playground/Contents.swift
@@ -25,9 +25,23 @@ func func4(count c: Int) {
 func4(count: 4)
 
 
-/// Default parameter
+/// Default parameters
 func func5(value: Int = 4) {
     print(value)
 }
 func5(2)
 func5()
+
+
+/// Constant parameters and variable parameters
+func func6(value: Int) {
+    // value += 3 // Error
+    print(value)
+}
+func6(1)
+
+func func7(var value: Int) {
+    value += 3 // OK
+    print(value)
+}
+func7(1)
