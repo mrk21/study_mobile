219f4afda987bd2c0ed925e4d174c1d3fae53310
Yuichi Murata <mrk21info+github@gmail.com>
2016-03-15 23:54:17 +0900

    swift_class: add

---
+++ swift_class/README.md
@@ -0,0 +1,5 @@
+# Swift Class
+
+## Refer to
+
+* [クラス | Swift言語を学ぶ](http://tea-leaves.jp/swift/content/クラス)
---
+++ swift_class/swift_class.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Class1 {
+    var value1 = 1
+    
+    func display() {
+        print(self.value1)
+    }
+}
+var valueClass1 = Class1()
+valueClass1.display()
