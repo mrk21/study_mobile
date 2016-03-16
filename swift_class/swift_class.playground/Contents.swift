374cea57c2c44f37e2328d83051f424233fcafb7
Yuichi Murata <mrk21info+github@gmail.com>
2016-03-16 23:58:55 +0900

    swift_class: add a code of assigning, initializer and deinitializer

--- swift_class/swift_class.playground/Contents.swift
+++ swift_class/swift_class.playground/Contents.swift
@@ -11,3 +11,23 @@ class Class1 {
 }
 var valueClass1 = Class1()
 valueClass1.display()
+
+var valueClass2 = valueClass1 // It assigns reference
+valueClass2.value1 = 2
+valueClass1.display()
+valueClass2.display()
+
+
+/// Initializer, Deinitializer
+class Class2 {
+    var value1: Int
+    
+    init(value1: Int) {
+        self.value1 = value1
+    }
+    
+    deinit {
+        print("deinit")
+    }
+}
+print(Class2(value1: 12).value1)
